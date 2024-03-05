cca6fe6c3502f810a8900923d72b12aae35d0cbc
larissa-carneiror <larissa-rodriguesc@hotmail.com>
2024-03-05 19:12:37 -0300

    .

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 // Declarando variáveis
-let nomeHeroi = "Capitão América "
-let xpHeroi = 10001
-let nivel = ""
+let nomeHeroi = "Capitão América";
+let xpHeroi = 10001;
+let nivel = "";
 
 // Implementando o if, else if
-if(xpHeroi <= 1000) {
-    nivel = "Bronze"
-}else if(xpHeroi >= 1001 && xpHeroi < 2000) {
-    nivel = "Ferro"
-}else if(xpHeroi >= 2001 && xpHeroi < 5000) {
-    nivel = "Prata"
-}else if(xpHeroi >= 5001 && xpHeroi < 7000) {
-    nivel = "Ouro"
-}else if(xpHeroi >= 7001 && xpHeroi < 8000) {
-    nivel = "Platina"
-}else if(xpHeroi >= 8001 && xpHeroi < 9000) {
-    nivel = "Ascendente"
-}else if(xpHeroi >= 9001 && xpHeroi < 10000) {
-    nivel = "Imortal"
-}else {
-    nivel = "Radiante"
+if (xpHeroi <= 1000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 1001 && xpHeroi < 2000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 2001 && xpHeroi < 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi < 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi < 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi < 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
 }
 
 // Imprimindo mensagem
-console.log("O Herói de nome " + nomeHeroi + "está no nível de " + nivel)
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)
